c8eab3193fefedf3ad014e9379ed0d790976bb1b
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-04-29 03:24:45 -0300

    Added server code: socket.io as dependency, youtube-dl script and io code in server.js.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
-var path = require('path');
+var pathutils = require('path');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Serve static
-app.use(express.static(path.join(__dirname, '../.tmp')));
-app.use(express.static(path.join(__dirname, '../app')));
+app.use(express.static(pathutils.join(__dirname, '../.tmp')));
+app.use(express.static(pathutils.join(__dirname, '../app')));
 
+// IO
+io.on('connection', function(socket){
+
+  socket.on('download', function(data){
+
+    // Generate download path
+    var path = pathutils.join(download_path, data.autor, data.titulo) + '.%(ext)s';
+
+    // Download
+    var downloading = download(data['url'], path);
+
+    downloading.on('data', function(data) {
+        socket.emit('statusUpdate', data);
+    })
+
+    downloading.on('end', function(data) {
+      socket.emit('statusUpdate', data);
+    })
+
+  })
+})
 
 // Listening
-var server = app.listen(3001, function () {
+server.listen(3001, function () {
 
   var host = server.address().address;
   var port = server.address().port;
---
+++ server/youtube-dl.js
@@ -0,0 +1,61 @@
+var EventEmitter = require('events').EventEmitter;
+var spawn = require('child_process').spawn;
+
+var download = function(url, path) {
+  var downloadRegex = /\[download\]\s{1,3}(\d{1,3}\.\d)% of .* at (.*) ETA (\d{2}:\d{2})/;
+  var eventEmitter = new EventEmitter();
+  command = 'youtube-dl';
+  args = ['--extract-audio', '--audio-format', 'mp3', '-o', path, url];
+  var proc = spawn(command, args);
+  proc.stdout.on('data', function(data) {
+    var response = data.toString().trim();
+
+    if (response.indexOf('[youtube]') != -1) {
+      // Preprocessing
+      var data = {
+        'url': url,
+        'status': 'preprocessing',
+        'details': response.slice(10)
+      }
+      eventEmitter.emit('data', data)
+    } else if (downloadRegex.test(response)) {
+      // Downloading
+      var info = downloadRegex.exec(response);
+      var data = {
+        'url': url,
+        'status': 'downloading',
+        'details': {
+          'percent': info[1],
+          'speed': info[2],
+          'ETA': info[3]
+        }
+      }
+      eventEmitter.emit('data', data);
+    } else if (response.indexOf('[ffmpeg]') != -1) {
+      //Postprocessing
+      var data = {
+        'url': url,
+        'status': 'postprocessing',
+        'details': 'Converting to MP3'
+      }
+      eventEmitter.emit('data', data);
+    }
+
+  })
+
+  proc.on('close', function(){
+    var data = {
+      'url': url,
+      'status': 'complete'
+    }
+    eventEmitter.emit('data', data);
+  })
+
+  proc.stderr.on('data', function(data){
+    console.log(data.toString())
+  })
+
+  return eventEmitter;
+}
+
+module.exports = download;
