7a1d92e015fb80dc75e4d9ece6788c695d328769
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-05-20 00:39:22 -0300

    jQuery is attached to the global scope and extended with semantic-ui plugins. semantic-ui CSS is loaded with require in main.jsx aswell.

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -2,6 +2,14 @@ var React = require('react');
 var mui = require('material-ui');
 var App = require('./components/App.jsx');
 
+// Make jQuery available in the global scope for plugins
+var $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+
+require('../semantic-ui/semantic.js');
+require('../semantic-ui/semantic.css');
+
 React.render(
   <App />,
   document.getElementById('app')
