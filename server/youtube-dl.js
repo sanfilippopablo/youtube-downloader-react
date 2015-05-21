107df209173beddd406f79f3f45fe63ae3b1bb0f
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-05-21 13:12:55 -0300

    youtube-dl.js now emits error data on error.

--- server/youtube-dl.js
+++ server/youtube-dl.js
@@ -5,10 +5,12 @@ var download = function(url, path) {
   console.log('Downloading')
   var downloadRegex = /\[download\]\s{1,3}(\d{1,3}\.\d)% of .* at (.*) ETA (\d{2}:\d{2})/;
   var eventEmitter = new EventEmitter();
+  var error = false;
   command = 'youtube-dl';
   args = ['--extract-audio', '--audio-format', 'mp3', '-o', path, url];
   var proc = spawn(command, args);
   proc.stdout.on('data', function(data) {
+    error = false;
     var response = data.toString().trim();
 
     if (response.indexOf('[youtube]') != -1) {
@@ -16,7 +18,10 @@ var download = function(url, path) {
       var data = {
         'URL': url,
         'status': 'preprocessing',
-        'details': response.slice(10)
+        'details': {
+          'percent': 0,
+          'description': response.slice(10)
+        }
       }
       eventEmitter.emit('data', data)
     } else if (downloadRegex.test(response)) {
@@ -26,7 +31,8 @@ var download = function(url, path) {
         'URL': url,
         'status': 'downloading',
         'details': {
-          'percent': info[1],
+          'description': 'Downloading video',
+          'percent': Number(info[1]),
           'speed': info[2],
           'ETA': info[3]
         }
@@ -37,7 +43,10 @@ var download = function(url, path) {
       var data = {
         'URL': url,
         'status': 'postprocessing',
-        'details': 'Converting to MP3'
+        'details': {
+          'percent': 100,
+          'description': 'Converting to MP3'
+        }
       }
       eventEmitter.emit('data', data);
     }
@@ -45,15 +54,32 @@ var download = function(url, path) {
   })
 
   proc.on('close', function(){
-    var data = {
-      'URL': url,
-      'status': 'complete'
+    if (!error) {
+      var data = {
+        'URL': url,
+        'status': 'complete',
+        'details': {
+          'percent': 100,
+          'description': 'Complete'
+        }
+      }
+      eventEmitter.emit('data', data);
     }
-    eventEmitter.emit('data', data);
   })
 
   proc.stderr.on('data', function(data){
-    console.log(data.toString())
+    // Error
+    error = true;
+    console.log(data.toString());
+    var data = {
+      'URL': url,
+      'status': 'error',
+      'details': {
+        'percent': 100,
+        'description': 'Error'
+      }
+    };
+    eventEmitter.emit('data', data);
   })
 
   return eventEmitter;
