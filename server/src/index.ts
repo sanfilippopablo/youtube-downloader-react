001372bad512bd7ff45158a3c343e1b23f97648f
Pablo Sanfilippo <pablo@slingshotaerospace.com>
2021-11-16 10:35:45 -0300

    Stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,12 @@ import express from "express";
 import { WebSocketServer } from "ws";
 import http from "http";
 import { download } from "./download";
+import path from "path";
 
 const app = express();
 const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 const wss = new WebSocketServer({ server });
 
