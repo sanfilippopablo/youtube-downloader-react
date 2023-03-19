fd01037c7737aa2a3146cce7879395b6a7c44df5
Pablo Sanfilippo <pablo@slingshotaerospace.com>
2023-03-19 12:01:17 -0300

    Refactor and tests

---
+++ server/tests/download.rs
@@ -0,0 +1,34 @@
+use std::{env, path::PathBuf};
+
+use tokio::sync::broadcast::{self};
+use youtube_downloader::{download, update_youtube_dl, DownloadArgs, DownloadType};
+
+#[tokio::test]
+async fn test_download() {
+    update_youtube_dl().await.ok();
+    let (tx, mut rx) = broadcast::channel(10);
+
+    download(
+        DownloadArgs {
+            download_type: DownloadType::Música,
+            url: String::from("https://www.youtube.com/watch?v=1qZHVQiRUJA"),
+            artist: String::from("Marcos Vidal"),
+            title: String::from("Tu Cara"),
+            cut: None,
+        },
+        tx,
+    );
+
+    let mut updates = Vec::new();
+
+    while let Ok(update) = rx.recv().await {
+        updates.push(update);
+    }
+
+    dbg!(updates);
+
+    let download_dir: PathBuf = env::var("MUSICA_DOWNLOAD_PATH").unwrap().into();
+    let expected_download_path = download_dir.join("Marcos Vidal").join("Tu Cara.mp3");
+    dbg!(&expected_download_path);
+    assert!(expected_download_path.exists());
+}
